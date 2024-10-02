f55df94f26eb3e11469cda73b29bd94d30ff9bab
SabaPangani <spangani37@gmail.com>
2024-10-02 04:17:37 +0400

    update dark mode

--- components/EmptyPage.tsx
+++ components/EmptyPage.tsx
@@ -5,8 +5,8 @@ export default function EmptyPage() {
   return (
     <div className="flex flex-col justify-center items-center mt-16">
       <Image src={empty} alt="Empty image" />
-      <h1 className="font-bold text-3xl mt-5">There is nothing here</h1>
-      <p className="text-gray text-center font-medium">
+      <h1 className="font-bold text-3xl mt-5 dark:text-white">There is nothing here</h1>
+      <p className="text-gray text-center font-medium dark:text-light-white">
         Create an invoice by clicking the <br />
         <span className="font-bold">New Invoice</span> button and get started
       </p>
